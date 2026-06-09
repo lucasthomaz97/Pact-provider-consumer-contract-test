e2cb91354459c25d1cfa0784224874dfed8180e9
LucasThomaz97 <lucasthomaz97@gmail.com>
2026-06-09 16:41:13 -0300

    test/provider/contract/new_tests_&_less_providerStates

--- provider/tests/users.spec.ts
+++ provider/tests/users.spec.ts
@@ -1,6 +1,12 @@
 import { Verifier } from '@pact-foundation/pact';
 import { describe, it } from 'vitest';
 import { createApp } from '../src/index';
+import { userRepository } from '../src/repository/UserRepository';
+
+const defaultUsers = [
+    { id: 1, name: "Lucas", email: "lucas.thomaz@example.com" },
+    { id: 2, name: "John", email: "john.doe@example.com" }
+];
 
 describe('Provider tests', () => {
     it('should validade the pact contract', async () => {
@@ -13,8 +19,17 @@ describe('Provider tests', () => {
             './pacts/Frontend-UsersAPI.json'
             ],
             stateHandlers: {
-                'user 1 exists': async () => {},
-                'users exist': async () => {}
+                'user exists': async () => {
+                    userRepository.seed([{ id: 1, name: "Lucas", email: "lucas.thomaz@example.com" }]);
+                },
+
+                'contains users': async () => {
+                    userRepository.seed(defaultUsers);
+                },
+
+                'repository is empty': async () => {
+                    userRepository.clear();
+                }
             }
         }).verifyProvider();
         } finally {
